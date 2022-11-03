55c697f4f72e913393d92fd336ffcf44dca8e708
ahmad_ammag <106084516+ahmadhannan-ammag@users.noreply.github.com>
2022-11-03 16:13:18 +0500

    Initial commit

---
+++ dist/js/themes/darkTheme/colors.js
@@ -0,0 +1,91 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.colors = void 0;
+/**
+ * Dark theme colors
+ */
+exports.colors = {
+    background: {
+        default: '#FFFFFF',
+        alternative: '#FFF',
+    },
+    text: {
+        default: '#000',
+        alternative: '#000',
+        muted: '#000',
+    },
+    icon: {
+        default: '#FFF',
+        alternative: '#FFF',
+        muted: '#FFF',
+    },
+
+
+    border: {
+        default: '#222884',
+        muted: '#222884',
+    },
+
+
+    overlay: {
+        default: '#222884',
+        inverse: '#FFF',
+
+        
+        alternative: '#222884', // pop background color address copied successfully
+    },
+    primary: {
+        default: '#222884', //selected tab text color
+        muted: '#222884', //selected tab
+        
+        
+        alternative: '#000',
+        inverse: '#FFF',
+        disabled: '#FFF',
+    },
+
+
+
+
+
+
+
+
+    
+    secondary: {
+        default: '#222884',
+        alternative: '#222884',
+        muted: '#222884',
+        inverse: '#222884',
+        disabled: '#222884',
+    },
+    error: {
+        default: '#FF0000',
+        alternative: '#FF0000',
+        muted: '#FF0000',
+        inverse: '#FF0000',
+        disabled: '#FF0000',
+    },
+    warning: {
+        default: '#222884',
+        alternative: '#222884',
+        muted: '#222884',
+        inverse: '#222884',
+        disabled: '#222884',
+    },
+    success: {
+        default: '#222884',
+        alternative: '#222884',
+        muted: '#222884',
+        inverse: '#222884',
+        disabled: '#222884',
+    },
+    info: {
+        default: '#222884',
+        alternative: '#222884',
+        muted: '#222884',
+        inverse: '#222884',
+        disabled: '#222884',
+    },
+};
+//# sourceMappingURL=colors.js.map
